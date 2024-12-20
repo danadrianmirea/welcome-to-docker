5415b0c26d1fe0470777d7f3c2b9a37af779767e
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-20 09:44:05 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -11,46 +11,8 @@ const App = () => {
       <header className="App-header">
         <h1 style={{ marginBottom: "0px" }}>Congratulations!!!</h1>
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
-          Docker hello world.
+          Test docker.
         </p>
-        <div>
-          <a
-            target="_blank"
-            href={
-              "https://twitter.com/intent/tweet?text=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-x-twitter"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-            target="_blank"
-            href={
-              "https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink
-            }
-            class="fa-brands fa-linkedin"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-            target="_blank"
-            href={
-              "https://reddit.com/submit?title=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-reddit"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-        </div>
       </header>
     </div>
   );
